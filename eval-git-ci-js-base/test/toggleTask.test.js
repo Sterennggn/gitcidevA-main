33909536fb9f272b19378cd0769e6a263417306a
SterennGougeon <sterenn.gougeon@gmail.com>
2025-10-01 15:11:57 +0200

    fonction toggleTask

---
+++ eval-git-ci-js-base/test/toggleTask.test.js
@@ -0,0 +1,9 @@
+test('toggleTask inverse done', () => {
+  const task = { done: false };
+
+  toggleTask(task);
+  expect(task.done).toBe(true);
+
+  toggleTask(task);
+  expect(task.done).toBe(false);
+});
